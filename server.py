90c08f389cad15627601ed2f3ea56625edca7167
SosnaK1 <jakub.sosna@icloud.com>
2019-09-15 15:20:53 +0200

    Improved server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 from threading import Thread
-import sys
 
 clients = {}
 addresses = {}
@@ -17,32 +16,63 @@ except socket.error as e:
     print(str(e))
 
 
-def acceptConnections():
-    id = 0
+players_positions = ["0:50,50", "1:100,100"]
+
+
+def accept_connections():
+    player_id = 0
     while True:
-        clientConnection, clientAddress = s.accept()
-        print(clientAddress + " has connected to the server!")
-        addresses[clientConnection] = clientAddress
+        client_connection, client_address = s.accept()
+        print(client_address + " has connected to the server!")
+        addresses[client_connection] = client_address
 
-        Thread(target=handleClient, args=(clientConnection, clientAddress, id)).start()
+        Thread(target=handle_client, args=(client_connection, player_id)).start()
 
-        id += 1
+        player_id += 1
 
 
-def handleClient(connection, address, id):
-    clients[connection] = id
-    print("Client got id: " + id)
+def handle_client(connection, player_id):
+    # TODO: add possibility for more than 2 players (proper id handling and sending positions)
 
-    connection.send(str.encode(id))
+    global players_positions
+
+    clients[connection] = player_id
+    print("Client got id: " + player_id)
+
+    connection.send(str.encode(player_id))
 
     while True:
+        try:
+            data = connection.recv(1024).decode("utf8")
+            if not data:
+                connection.send(str.encode("Bye"))
+                break
+            else:
+                print("Received: " + data)
+                arr = data.split(":")
+                sender_id = int(arr[0])
+                players_positions[sender_id] = data
 
+                if player_id == 0:
+                    other_id = 1
+                else:                       # player_id == 1:
+                    other_id = 0
 
+                reply = players_positions[other_id][:]
+                print("Sending: " + reply)
+
+            connection.sendall(str.encode(reply))
+        except socket.error as err:
+            print("Error while receiving position or sending it back" + str(err))
+            break
+
+        print("Connection closed.")
+        connection.close()
 
 
 if __name__ == '__main__':
     s.listen(5)
     print("Waiting for connection...")
-    t1 = Thread(target=acceptConnections)
+    t1 = Thread(target=accept_connections)
     t1.start()
     t1.join()
